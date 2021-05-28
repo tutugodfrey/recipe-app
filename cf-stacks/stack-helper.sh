fdf22c84ee30b612ef158e47f373ba160a03a51d
tutugodfrey <godfrey_tutu@yahoo.com>
2021-05-28 16:11:21 +0100

    App Stack
    - Add the application server stack

--- cf-stacks/stack-helper.sh
+++ cf-stacks/stack-helper.sh
@@ -8,6 +8,8 @@ aws cloudformation create-stack --stack-name recipe-s3-bucket-stack --template-b
 # Create vpc stack
 aws cloudformation create-stack --stack-name recipe-vpc-stack --template-body file://cf-stacks/vpc-stack.yml --profile myg
 
+# Create the app stack
+aws cloudformation create-stack --region us-east-1 --stack-name recipe-app-stack --template-body file://cf-stacks/app-stack.yml --parameters ParameterKey=KeyPair,ParameterValue=cloud-arch --capabilities CAPABILITY_IAM --profile myg
 
 # Delete the stacks
 
@@ -15,3 +17,4 @@ delete-stack --stack-name recipe-vpc-stack  --profile myg
 
 # Delete the VPC
 aws cloudformation delete-stack --stack-name recipe-s3-bucket-stack --profile myg
+
